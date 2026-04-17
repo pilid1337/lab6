9415fa013c030977449e54c5306d3af5d6cc555d
pilid1337 <aalmaz_a@mail.ru>
2026-04-17 12:19:27 +0500

    minor bugfix

---
+++ task1/VipPatron.cs
@@ -0,0 +1,50 @@
+public class VipPatron : BarPatron
+{
+    private int _remainingVipDays;
+
+    public VipPatron(bool sober, bool happy, int remainingDays) 
+    : base(sober, happy)
+    {
+        _remainingVipDays = remainingDays >= 0 ? remainingDays : 0;
+    }
+
+    public VipPatron(VipPatron other) : base(other)
+    {
+        _remainingVipDays = other._remainingVipDays;
+    }
+
+    private int RemainingVipDays
+    {
+        get { 
+            return _remainingVipDays; 
+        }
+        set 
+        { 
+            _remainingVipDays = value >= 0 ? value : 0; 
+        }
+    }
+
+    public bool CanEnterVipLounge()
+    {
+        return _remainingVipDays > 0;
+    }
+
+    public int UseVipDay()
+    {
+        if (_remainingVipDays > 0)
+        {
+            _remainingVipDays--;
+        }
+        return _remainingVipDays;
+    }
+
+    public void ExtendVipSubscription(int days)
+    {
+        _remainingVipDays += days;
+    }
+
+    public override string ToString()
+    {
+        return $"VipPatron: трезвый={IsSober}, довольный={IsHappy}, дней подписки={_remainingVipDays}";
+    }
+}
